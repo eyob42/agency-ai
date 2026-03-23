f5725527e0022dcc6171146db512c3eeb00fc289
eyob42 <eyobadigeh07@gmail.com>
2026-03-23 03:27:45 +0300

    feat(ui): Implement contact form, footer, and toast notifications

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,24 @@ import TrustedBy from './Component/TrustedBy'
 import Services from './Component/Services'
 import OurWork from './Component/OurWork'
 import Teams from './Component/Teams'
+import ContactUs from './Component/ContactUs'
+import {Toaster} from 'react-hot-toast'
+import Footer from './Component/Footer'
 
 const App = () => {
 
   const [theme, setTheme]= useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
   return (
     <div className='dark:bg-black relative'>
+      <Toaster />
       <Navbar theme={theme}  setTheme={setTheme} />
       <Hero />
       <TrustedBy />
       <Services />
       <OurWork />
       <Teams />
+      <ContactUs />
+      <Footer theme={theme}/>
       
     </div>
   )
